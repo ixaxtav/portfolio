d64d86432ad05f596fe0de3cdf820edceb541283
Ixax Tavarez <ixaxtav@gmail.com>
2023-08-08 03:59:01 -0400

    update elevent and add doc build

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts/MPLUSRounded1c-Regular.ttf");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  return {
+    dir: {
+      output: "docs", // Output directory for the generated files
+    },
+  };
 };
